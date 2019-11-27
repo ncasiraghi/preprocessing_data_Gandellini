f34559a7c0f00762c0dd42320a4ebe0b4867d9b5
Nicola <casiraghinicola@gmail.com>
2019-11-27 11:48:35 +0100

    configure files removed

--- RunBWA_mem_Config_Sample11B_run_20141210.R
+++
@@ -1,7 +0,0 @@
-fastqFolder <- "/CIBIO/sharedCO/Exome_seq/Gandellini/alignment_test_Sample_11B/20141210_gandellini" 
-
-number_of_threads = 20
-
-ReferenceFasta = "/elaborazioni/sharedCO/Home_casiraghi/Reference_b37/human_g1k_v37.fasta"
-
-BWA_mem = "/usr/bin/bwa mem -M"
